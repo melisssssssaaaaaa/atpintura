4a2b356721c54ab8d8a3b3fb9bcd6d9208385391
Melissa <91746003+melpompom@users.noreply.github.com>
2022-10-21 09:02:58 -0300

    Add files via upload

---
+++ atcuboMain.java
@@ -0,0 +1,11 @@
+package pintura;
+
+public class atcuboMain {
+
+	public static void main(String[] args) {
+
+		atcubo cubo = new atcubo(107.45, 12.50, 2); //(lado, rendimento, tipo)
+		System.out.println(cubo);
+	
+}
+}
---
+++ atpiramideMain.java
@@ -0,0 +1,12 @@
+package pintura;
+
+public class atpiramideMain {
+
+		public static void main(String[] args) {
+			
+			atpiramide p = new atpiramide(13.89, 41.87, 3); //(ab, h, tipo)
+			System.out.println(p);
+			
+		}
+	}
+
